6d43287b87ab4e5b17d74fe194e3a9f21b825a4c
Azonaz <malinina.a@gmail.com>
2025-07-03 18:07:16 +0300

    Initial Commit

---
+++ StripedProgressBarTests/StripedProgressBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  StripedProgressBarTests.swift
+//  StripedProgressBarTests
+//
+//  Created by Админ on 03/07/2025.
+//
+
+import Testing
+@testable import StripedProgressBar
+
+struct StripedProgressBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
